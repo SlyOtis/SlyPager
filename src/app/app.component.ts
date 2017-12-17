5f78b668bfd3a4f69a6153022689fd905a037c0d
Martin Ottersland <ottini93@gmail.com>
2017-12-17 21:12:26 +0100

    Updated example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {SlyPagerPageComponent} from './sly-pager-page/sly-pager-page.component';
-import {SlyPagerConfig} from "./sly-pager/sly-pager.component";
+import {SlyPagerConfig} from './sly-pager/sly-pager.component';
+import * as moment from "moment";
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,9 @@ import {SlyPagerConfig} from "./sly-pager/sly-pager.component";
   entryComponents: [SlyPagerPageComponent]
 })
 export class AppComponent {
-  refMoment
+  refMoment = moment();
   config: SlyPagerConfig = {
-    startIndex: {index: 2, window: {id: 0, size: 10}},
+    startIndex: {index: this.refMoment.isoWeek() - 1, window: {id: 0, size: this.refMoment.isoWeeksInYear()}},
     onCreateComponent: (index) => {
       return SlyPagerPageComponent;
     },
@@ -22,13 +23,19 @@ export class AppComponent {
       return {index: 10 - overshoot, window: {id: currWindow.id - 1, size: 10}};
     },
     scrollDirection: Hammer.DIRECTION_HORIZONTAL,
-    onBindComponent: (index, component) => {
+    onBindComponent: (index, component: SlyPagerPageComponent) => {
       console.log(index);
     },
     mode: 'infinite',
     blockOnAnimate: true,
-    onIndexChanged: index => {
-
+    onIndexChanged: (index, component: SlyPagerPageComponent) => {
+      let week = moment(this.refMoment).isoWeek(index.index);
+      if (index.window.id > 0) {
+        week = week.add(index.window.id, 'years');
+      } else if (index.window.id < 0) {
+        week = week.subtract(Math.abs(index.window.id), 'years');
+      }
+      component.setText(week.toISOString());
     }
   };
 }
